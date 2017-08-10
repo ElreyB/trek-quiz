311ac9ad1d5f954bc9d5aec106cca1b27b00bf5d
elrey belmonti and snow vilay <elrey@>
2017-08-10 16:38:53 -0700

    fix hide function

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-  $("#reset").click(function(){
-    HTMLFormElement.reset()
-    $("uhura").hide();
-    $("scotty").hide();
-    $("spock").hide();
-    $("kirk").hide();
-  });
+  // $("#reset").click(function(){
+  //   // HTMLFormElement.reset()
+  //   $("#uhura").hide();
+  //   $("#scotty").hide();
+  //   $("#spock").hide();
+  //   $("#kirk").hide();
+  // });
   $(".quiz").submit(function(event){
   var seasonInput = parseInt($("#season").val());
   var animalInput = parseInt($("#animal").val());
@@ -16,24 +16,24 @@ $(document).ready(function(){
 
     if (result <= 4){
       $("#kirk").show();
-      // $("uhura").hide();
-      // $("scotty").hide();
-      // $("spock").hide();
+      $("#uhura").hide();
+      $("#scotty").hide();
+      $("#spock").hide();
     } else if (result > 4 && result <= 8){
       $("#scotty").show();
-      // $("uhura").hide();
-      // $("kirk").hide();
-      // $("spock").hide();
+      $("#uhura").hide();
+      $("#kirk").hide();
+      $("#spock").hide();
     }else if (result > 8 && result <= 12){
       $("#uhura").show();
-      // $("kirk").hide();
-      // $("scotty").hide();
-      // $("spock").hide();
+      $("#kirk").hide();
+      $("#scotty").hide();
+      $("#spock").hide();
     }else if (result > 12){
       $("#spock").show();
-      // $("uhura").hide();
-      // $("scotty").hide();
-      // $("kirk").hide();
+      $("#uhura").hide();
+      $("#scotty").hide();
+      $("#kirk").hide();
     }else{
       $("span").show();
     }
